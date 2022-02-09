02b1297ef746ad3bc3602c4efe4c7ab8ed203481
CHRIS L <s160419046@student.ubaya.ac.id>
2022-02-09 14:27:17 +0700

    dosen added by chris

---
+++ app/src/main/java/com/ubaya/native160419094/Dosen.kt
@@ -0,0 +1,7 @@
+package com.ubaya.native160419094
+
+class Dosen(var nrp:String,var name:String) {
+    override fun toString(): String {
+        return "$nrp bernama $name sudah dapat diakses"
+    }
+}
